9b115f079704bc54668ebb53d3d24281d2ecb5c5
你的名字 <@example.com>
2026-04-13 14:56:04 +0800

    添加内容

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
+const redirectPath = sessionStorage.getItem("redirectPath");
+if (redirectPath) {
+  sessionStorage.removeItem("redirectPath");
+  router.replace(redirectPath);
+}
